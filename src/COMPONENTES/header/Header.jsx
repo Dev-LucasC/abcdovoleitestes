20f0329f6bb4ca379d2ac32170b55585fc0268ca
Dev-LucasC <lucaconceicao@hotmail.com>
2023-02-27 12:04:49 -0300

    TesteHader

--- src/COMPONENTES/header/Header.jsx
+++ src/COMPONENTES/header/Header.jsx
@@ -4,79 +4,71 @@ import Logo from './logo.png'
 
 class MobileNavbar {
     constructor(mobileMenu, navList, navLinks) {
-      this.mobileMenu = document.querySelector(mobileMenu);
-      this.navList = document.querySelector(navList);
-      this.navLinks = document.querySelectorAll(navLinks);
-      this.activeClass = "active";
-  
-      this.handleClick = this.handleClick.bind(this);
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
     }
-  
+
     animateLinks() {
-      this.navLinks.forEach((link, index) => {
-        link.style.animation
-          ? (link.style.animation = "")
-          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-              index / 7 + 0.3
-            }s`);
-      });
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3
+                    }s`);
+        });
     }
-  
+
     handleClick() {
-      this.navList.classList.toggle(this.activeClass);
-      this.mobileMenu.classList.toggle(this.activeClass);
-      this.animateLinks();
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
     }
-  
+
     addClickEvent() {
-      this.mobileMenu.addEventListener("click", this.handleClick);
+        this.mobileMenu.addEventListener("click", this.handleClick);
     }
-  
+
     init() {
-      if (this.mobileMenu) {
-        this.addClickEvent();
-      }
-      return this;
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
     }
-  }
-  
-  const mobileNavbar = new MobileNavbar(
+}
+
+const mobileNavbar = new MobileNavbar(
     ".mobile-menu",
-    ".header-nav",
-    ".header-nav li",
-  );
-  mobileNavbar.init();
+    ".nav-list",
+    ".nav-list li",
+);
+mobileNavbar.init();
 
 const Header = () => {
-  return (
-    <header>
-        
-        <div className="header-logo">
-            <img src={Logo} alt="" />
-        </div>
-      
-        <div className="header-nav">
-            <ul>
-                <li>
-                    <a href="">Inicio |</a>
-                </li>
-                <li>
-                    <a href="">Noticias |</a>
-                </li>
-                <li>
-                    <a href="">Prestação de contas |</a>
-                </li>
-                <li>
-                    <a href="">Equipes |</a>
-                </li>
-                <li>
-                    <a href="">Calendário |</a>
-                </li>
-            </ul>
-        </div>
-        <script src="Header.jsx"></script>
-    </header>
-  )
+    return (
+        <header>
+            <nav>
+                <div className="imagem">
+                    <img src={Logo} alt="" />
+                </div>
+                <div class="mobile-menu">
+                    <div class="line1"></div>
+                    <div class="line2"></div>
+                    <div class="line3"></div>
+                </div>
+                <ul class="nav-list">
+                    <li><a href="#">Início</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Projetos</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav>
+        </header>
+
+
+    )
 }
 
 export default Header
